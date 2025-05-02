b6d5e2a6ac32b2d44f7936c3c4990da8faf0013d
Jason Nguyen <jnguyen7473@gmail.com>
2025-05-01 21:56:40 -0700

    Added default values and representation

--- app/models.py
+++ app/models.py
@@ -8,10 +8,17 @@ class CharacterBase(db.Model):
 
     base_hp = db.Column(db.Integer)
     base_def = db.Column(db.Integer)
-    base_crit_rate = db.Column(db.Integer)
-    base_crit_dmg = db.Column(db.Integer)
-    base_pen_ratio = db.Column(db.Integer)
+    base_atk = db.Column(db.Integer)
+    base_crit_rate = db.Column(db.Integer, default = 0)
+    base_crit_dmg = db.Column(db.Integer, default = 50)
+    base_pen_ratio = db.Column(db.Integer, default = 0)
     base_impact = db.Column(db.Integer)
     base_anomaly_mastery = db.Column(db.Integer)
     base_anomaly_proficiency = db.Column(db.Integer)
-    base_energy = db.Column(db.Integer)
+    base_energy_regen = db.Column(db.Integer, default = 1.2)
+
+    def __repr__(self):
+        return """{0}
+        {1}, {2}, {3},
+        hp {4} def {5} atk {6}
+        """.format(self.name, self.attribute, self.specialty, self.faction, self.base_hp, self.base_def, self.base_atk)
