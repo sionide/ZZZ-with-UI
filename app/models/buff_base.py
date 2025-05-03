2d1853b28beddb738516e37fedabe6a1ee2cbf6f
Jason Nguyen <jnguyen7473@gmail.com>
2025-05-02 17:59:39 -0700

    BuffBase is now polymorphic and only contains id and type columns now
    The rest of the buffs will be moved to subclasses of buff_base for the
    sake of viewing the app.db file.
    
    It will be repettitive and take up more space this way but it makes
    reading the app.db file far more intuitive

--- app/models/buff_base.py
+++ app/models/buff_base.py
@@ -2,23 +2,8 @@ from app import db
 
 class BuffBase(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String)
+    type = db.Column(db.String)
 
-    unconditional_atk_percent = db.Column(db.Integer, default = 0)
-    unconditional_atk_flat = db.Column(db.Integer, default = 0)
-    conditional_atk_percent = db.Column(db.Integer, default = 0)
-    conditional_atk_flat = db.Column(db.Integer, default = 0)
-    crit_rate = db.Column(db.Integer, default = 0)
-    crit_dmg = db.Column(db.Integer, default = 0)
-    bonus_dmg_attribute = db.Column(db.String, default = None)
-    bonus_dmg = db.Column(db.Integer, default = 0)
-    pen_ratio = db.Column(db.Integer, default = 0)
-    pen_flat = db.Column(db.Integer, default = 0)
-    anomaly_proficiency = db.Column(db.Integer, default = 0)
-    anomaly_mastery = db.Column(db.Integer, default = 0)
-
-    def_reduction = db.Column(db.Integer, default = 0)
-    res_ignore = db.Column(db.Integer, default = 0)
-    dmg_taken = db.Column(db.Integer, default = 0)
-    out_of_stun = db.Column(db.Boolean, default = False)
-    stun_dmg = db.Column(db.Integer, default = 0)
+    __mapper_args__ = {
+        'polymorphic_on' : type,
+        'polymorphic_identity': 'Base' }
